a6aa0a44cfdea234e78e419a7e3b9831e35a4809
Varshini Shree <117148310+VarshiniShreeV@users.noreply.github.com>
2025-12-02 07:34:37 +0530

    Day-1

---
+++ Coding/Arrays/Leetcode_1346.java
@@ -0,0 +1,14 @@
+class Leetcode_1346 {
+    public boolean checkIfExist(int[] arr) {
+        int n = arr.length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==j) continue;
+                if(arr[i]==2*arr[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
